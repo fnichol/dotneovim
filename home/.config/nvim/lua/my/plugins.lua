6cb4d9e1c1301d8c3688812c3df9526193ea109b
Fletcher Nichol <fnichol@nichol.ca>
2022-01-23 11:06:34 -0700

    style(plugins): apply formatting conventions
    Signed-off-by: Fletcher Nichol <fnichol@nichol.ca>

--- home/.config/nvim/lua/my/plugins.lua
+++ home/.config/nvim/lua/my/plugins.lua
@@ -1,43 +1,43 @@
 return function(use)
   -- Packer manages itself
-  use "wbthomason/packer.nvim"
+  use("wbthomason/packer.nvim")
   -- An implementation of the Popup API from Vim in NeoVim
-  use "nvim-lua/popup.nvim"
+  use("nvim-lua/popup.nvim")
   -- Useful Lua functions used by many plugins
-  use "nvim-lua/plenary.nvim"
+  use("nvim-lua/plenary.nvim")
 
   --
   -- ## Color schemes
   --
   -- Neovim plugin for building base16 colorschemes with support for Neovim's
   -- builtin LSP and Treesitter
-  use "RRethy/nvim-base16"
+  use("RRethy/nvim-base16")
   -- Remove all background colors to make nvim transparent
-  use "xiyaowong/nvim-transparent"
+  use("xiyaowong/nvim-transparent")
 
   --
   -- ## Completions
   --
   -- A completion plugin for NeoVim coded in Lua.
-  use { "hrsh7th/nvim-cmp", requires = { "L3MON4D3/LuaSnip", "rafamadriz/friendly-snippets" } }
+  use({ "hrsh7th/nvim-cmp", requires = { "L3MON4D3/LuaSnip", "rafamadriz/friendly-snippets" } })
   -- nvim-cmp source for buffer words
-  use "hrsh7th/cmp-buffer"
+  use("hrsh7th/cmp-buffer")
   -- nvim-cmp source for path
-  use "hrsh7th/cmp-path"
+  use("hrsh7th/cmp-path")
   -- nvim-cmp source for Vim's cmdline
-  use "hrsh7th/cmp-cmdline"
+  use("hrsh7th/cmp-cmdline")
   -- nvim-cmp source for NeoVim Lua
-  use "hrsh7th/cmp-nvim-lua"
+  use("hrsh7th/cmp-nvim-lua")
   -- nvim-cmp source for LuaSnip snippets
-  use "saadparwaiz1/cmp_luasnip"
+  use("saadparwaiz1/cmp_luasnip")
   -- Visual Studio Code-like pictograms for NeoVim LSP completion items
-  use "onsails/lspkind-nvim"
+  use("onsails/lspkind-nvim")
 
   --
   -- ## Snippets
   --
   -- Snippet Engine for Neovim written in Lua
-  use "L3MON4D3/LuaSnip"
+  use("L3MON4D3/LuaSnip")
   -- Set of preconfigured snippets for different languages
-  use "rafamadriz/friendly-snippets"
+  use("rafamadriz/friendly-snippets")
 end
