1010ad89cbd28652b61fb6c79e7d7a629b038bab
Fletcher Nichol <fnichol@nichol.ca>
2022-01-22 18:05:19 -0700

    feat: add completions via nvim-cmp
    Signed-off-by: Fletcher Nichol <fnichol@nichol.ca>

---
+++ home/.config/nvim/lua/my/cmp.lua
@@ -0,0 +1,125 @@
+-- Completion sources and ordering
+local sources = {
+  { name = "nvim_lua" },
+  { name = "luasnip" },
+  { name = "buffer" },
+  { name = "path" },
+}
+
+-- Completion menu source names
+local menu_source_names = {
+  buffer = "[Buffer]",
+  luasnip = "[Snippet]",
+  nvim_lsp = "[LSP]",
+  nvim_lua = "[Lua]",
+  path = "[Path]",
+}
+
+local ok, cmp = pcall(require, "cmp")
+if not ok then
+  vim.notify("[cmp] failed to require 'cmp'", WARN)
+  return
+end
+
+local ok, luasnip = pcall(require, "luasnip")
+if not ok then
+  vim.notify("[cmp] failed to require 'luasnip'", WARN)
+  return
+end
+
+local ok, lspkind = pcall(require, "lspkind")
+if not ok then
+  vim.notify("[cmp] failed to require 'lspkind'", WARN)
+  return
+end
+
+require("luasnip/loaders/from_vscode").lazy_load()
+
+-- Checks if the character preceding the cursor is a space character
+-- @return boolean true if it is a space character and false otherwise
+local function check_backspace()
+  local col = vim.fn.col(".") - 1
+  return col == 0 or vim.fn.getline("."):sub(col, col):match("%s")
+end
+
+-- Sets up a "Super-Tab" like mapping for the <Tab> key
+-- @fallback function fallback function to default behavior
+local function tab_mapping(fallback)
+  if cmp.visible() then
+    cmp.select_next_item()
+  elseif luasnip.expandable() then
+    luasnip.expand()
+  elseif luasnip.expand_or_jumpable() then
+    luasnip.expand_or_jump()
+  elseif check_backspace() then
+    fallback()
+  else
+    fallback()
+  end
+end
+
+-- Sets up a "Super-Tab" like mapping for the <S-Tab> key
+-- @fallback function fallback function to default behavior
+local function shift_tab_mapping(fallback)
+  if cmp.visible() then
+    cmp.select_prev_item()
+  elseif luasnip.jumpable(-1) then
+    luasnip.jump(-1)
+  else
+    fallback()
+  end
+end
+
+cmp.setup({
+  snippet = {
+    -- A snippet engine is required
+    expand = function(args)
+      luasnip.lsp_expand(args.body)
+    end,
+  },
+  mapping = {
+    -- Use C+up/down motion keys to navigate the previous and next items
+    ["<C-k>"] = cmp.mapping.select_prev_item(),
+    ["<C-j>"] = cmp.mapping.select_next_item(),
+    -- Use C+left/right motion keys to navigate the previous and next items
+    ["<C-h>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
+    ["<C-l>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
+    -- Trigger the completion dialog/mode without having to type any leading text
+    ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
+    -- Remove the default <C-y> mapping
+    ["<C-y>"] = cmp.config.disable,
+    -- Exit the completion dialog/mode
+    ["<C-e>"] = cmp.mapping({ i = cmp.mapping.abort(), c = cmp.mapping.close() }),
+    -- Accept currently selected item. If none is selected, `select` the first
+    -- item
+    -- Set `select` to `false` to only confirm the explicitly selected items
+    ["<CR>"] = cmp.mapping.confirm({ select = true }),
+    -- Setup a "Super-Tab" like mapping
+    ["<Tab>"] = cmp.mapping(tab_mapping, { "i", "s" }),
+    -- Setup a reverse "Super-Tab" like mapping
+    ["<S-Tab>"] = cmp.mapping(shift_tab_mapping, { "i", "s" }),
+  },
+  formatting = {
+    fields = { "kind", "abbr", "menu" },
+    format = lspkind.cmp_format({
+      -- Don't show text alongside icons
+      with_text = false,
+      -- Prevent the popup from showing more than the given chanracters
+      maxwidth = 80,
+      -- Display sources of completions
+      menu = menu_source_names,
+    }),
+  },
+  sources = sources,
+  confirm_opts = {
+    behavior = cmp.ConfirmBehavior.Replace,
+    select = false,
+  },
+  documentation = {
+    border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
+  },
+  experimental = {
+    ghost_text = false,
+    native_menu = false,
+  },
+})
--- home/.config/nvim/lua/my/plugins.lua
+++ home/.config/nvim/lua/my/plugins.lua
@@ -14,4 +14,30 @@ return function(use)
   use "RRethy/nvim-base16"
   -- Remove all background colors to make nvim transparent
   use "xiyaowong/nvim-transparent"
+
+  --
+  -- ## Completions
+  --
+  -- A completion plugin for NeoVim coded in Lua.
+  use { "hrsh7th/nvim-cmp", requires = { "L3MON4D3/LuaSnip", "rafamadriz/friendly-snippets" } }
+  -- nvim-cmp source for buffer words
+  use "hrsh7th/cmp-buffer"
+  -- nvim-cmp source for path
+  use "hrsh7th/cmp-path"
+  -- nvim-cmp source for Vim's cmdline
+  use "hrsh7th/cmp-cmdline"
+  -- nvim-cmp source for NeoVim Lua
+  use "hrsh7th/cmp-nvim-lua"
+  -- nvim-cmp source for LuaSnip snippets
+  use "saadparwaiz1/cmp_luasnip"
+  -- Visual Studio Code-like pictograms for NeoVim LSP completion items
+  use "onsails/lspkind-nvim"
+
+  --
+  -- ## Snippets
+  --
+  -- Snippet Engine for Neovim written in Lua
+  use "L3MON4D3/LuaSnip"
+  -- Set of preconfigured snippets for different languages
+  use "rafamadriz/friendly-snippets"
 end
