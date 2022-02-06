8fa42c658b8a71013c25aff3674597f7ae5c5771
Fletcher Nichol <fnichol@nichol.ca>
2022-02-06 14:54:09 -0700

    fix: ensure that NeoVim's `pwd` is *not* updated when opening files
    Signed-off-by: Fletcher Nichol <fnichol@nichol.ca>

--- home/.config/nvim/lua/my/nvim-tree.lua
+++ home/.config/nvim/lua/my/nvim-tree.lua
@@ -48,7 +48,7 @@ nvim_tree.setup({
   auto_close = true,
   open_on_tab = false,
   hijack_cursor = false,
-  update_cwd = true,
+  update_cwd = false,
   update_to_buf_dir = {
     enable = true,
     auto_open = true,
@@ -64,7 +64,7 @@ nvim_tree.setup({
   },
   update_focused_file = {
     enable = true,
-    update_cwd = true,
+    update_cwd = false,
     ignore_list = {},
   },
   system_open = {
