b4c3321db838140484f6706a4c01ad810435a5b0
Fletcher Nichol <fnichol@nichol.ca>
2022-02-16 11:11:35 -0700

    refactor(which_key): add prefix support for mappings
    This change allows for easy global mappings that aren't strictly
    `<leader>` prefixed mappings.
    
    Signed-off-by: Fletcher Nichol <fnichol@nichol.ca>

--- home/.config/nvim/lua/my/which_key.lua
+++ home/.config/nvim/lua/my/which_key.lua
@@ -7,151 +7,153 @@ end
 -- Usage: https://github.com/folke/which-key.nvim
 
 local mappings = {
-  ["a"] = { "<cmd>Alpha<CR>", "Alpha" },
-  ["b"] = { "<cmd>lua require('my.telescope').list_buffers()<CR>", "Buffers" },
-  ["c"] = { "<cmd>Bdelete!<CR>", "Close Buffer" },
-  ["e"] = { "<cmd>NvimTreeToggle<CR>", "Explorer" },
-  ["f"] = { "<cmd>Telescope find_files<CR>", "Find Files" },
-  ["h"] = { "<cmd>nohlsearch<CR>", "No Highlight" },
-  ["r"] = { "<cmd>Telescope oldfiles<CR>", "Open Recent File" },
+  ["<leader>"] = {
+    a = { "<cmd>Alpha<CR>", "Alpha" },
+    b = { "<cmd>lua require('my.telescope').list_buffers()<CR>", "Buffers" },
+    c = { "<cmd>Bdelete!<CR>", "Close Buffer" },
+    e = { "<cmd>NvimTreeToggle<CR>", "Explorer" },
+    f = { "<cmd>Telescope find_files<CR>", "Find Files" },
+    h = { "<cmd>nohlsearch<CR>", "No Highlight" },
+    R = { "<cmd>Telescope oldfiles<CR>", "Open Recent File" },
 
-  --
-  -- ## Debugging
-  --
-  d = {
-    name = "Debugging",
-    t = { "<cmd>lua require('dapui').toggle()<CR>", "Toggle DAP UI" },
+    --
+    -- ## Debugging
+    --
+    d = {
+      name = "Debugging",
+      t = { "<cmd>lua require('dapui').toggle()<CR>", "Toggle DAP UI" },
 
-    b = { "<cmd>lua require('dap').toggle_breakpoint()<CR>", "Toggle Breakpoint" },
-    B = {
-      "<cmd>lua require('dap').set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>",
-      "Breakpoint Condition",
+      b = { "<cmd>lua require('dap').toggle_breakpoint()<CR>", "Toggle Breakpoint" },
+      B = {
+        "<cmd>lua require('dap').set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>",
+        "Breakpoint Condition",
+      },
+      L = {
+        "<cmd>lua require('dap').set_breakpoint(nil, nil, vim.fn.input('Log point message: '))<CR>",
+        "Set Log Point",
+      },
+      T = { "<cmd>lua require('dap').list_breakpoints()<CR>", "Lists All Breakpoints" },
+      x = { "<cmd>lua require('dap').clear_breakpoints()<CR>", "Clears All Breakpoints" },
+
+      c = { "<cmd>lua require('dap').continue()<CR>", "Continue" },
+      C = { "<cmd>lua require('dap').run_to_cursor()<CR>", "Run To Cursor" },
+
+      o = { "<cmd>lua require('dap').step_over()<CR>", "Step Over" },
+      i = { "<cmd>lua require('dap').step_into()<CR>", "Step Into" },
+      O = { "<cmd>lua require('dap').step_into()<CR>", "Step Out" },
+
+      j = { "<cmd>lua require('dap').down()<CR>", "Go Down Stacktrace" },
+      k = { "<cmd>lua require('dap').up()<CR>", "Go Up Stacktrace" },
+
+      l = { "<cmd>lua require('dap').run_last()<CR>", "Run Last" },
+      r = { "<cmd>lua require('dap').repl.toggle()<CR>", "Toggle REPL" },
     },
-    L = {
-      "<cmd>lua require('dap').set_breakpoint(nil, nil, vim.fn.input('Log point message: '))<CR>",
-      "Set Log Point",
+
+    --
+    -- ## Git
+    --
+    g = {
+      name = "Git",
+      f = { "<cmd>G<CR>", "Fugitive" },
+      g = { "<cmd>lua _GITUI_TOGGLE()<CR>", "GitUI" },
+      j = { "<cmd>lua require('gitsigns').next_hunk()<CR>", "Next Hunk" },
+      k = { "<cmd>lua require('gitsigns').prev_hunk()<CR>", "Prev Hunk" },
+      l = { "<cmd>lua require('gitsigns').blame_line()<CR>", "Blame" },
+      p = { "<cmd>lua require('gitsigns').preview_hunk()<CR>", "Preview Hunk" },
+      r = { "<cmd>lua require('gitsigns').reset_hunk()<CR>", "Reset Hunk" },
+      R = { "<cmd>lua require('gitsigns').reset_buffer()<CR>", "Reset Buffer" },
+      s = { "<cmd>lua require('gitsigns').stage_hunk()<CR>", "Stage Hunk" },
+      u = { "<cmd>lua require('gitsigns').undo_stage_hunk()<CR>", "Undo Stage Hunk" },
+      o = { "<cmd>Telescope git_status<CR>", "Open Changed File" },
+      b = { "<cmd>Telescope git_branches<CR>", "Checkout Branch" },
+      c = { "<cmd>Telescope git_commits<CR>", "Checkout Commit" },
+      d = { "<cmd>Gitsigns diffthis HEAD<CR>", "Diff" },
     },
-    T = { "<cmd>lua require('dap').list_breakpoints()<CR>", "Lists All Breakpoints" },
-    x = { "<cmd>lua require('dap').clear_breakpoints()<CR>", "Clears All Breakpoints" },
 
-    c = { "<cmd>lua require('dap').continue()<CR>", "Continue" },
-    C = { "<cmd>lua require('dap').run_to_cursor()<CR>", "Run To Cursor" },
+    --
+    -- ## LSP
+    --
+    l = {
+      name = "LSP",
+      i = { "<cmd>LspInfo<CR>", "Info" },
+      I = { "<cmd>LspInstallInfo<CR>", "Installer Info" },
+      -- For activated buffer-local mappings, see `lua/my/lsp/handlers.lua`
+    },
 
-    o = { "<cmd>lua require('dap').step_over()<CR>", "Step Over" },
-    i = { "<cmd>lua require('dap').step_into()<CR>", "Step Into" },
-    O = { "<cmd>lua require('dap').step_into()<CR>", "Step Out" },
+    --
+    -- ## Terminal
+    --
+    m = {
+      name = "Terminal",
+      b = { "<cmd>lua _HTOP_TOGGLE()<CR>", "Htop" },
+      f = { "<cmd>ToggleTerm direction=float<CR>", "Float" },
+      h = { "<cmd>ToggleTerm size=10 direction=horizontal<CR>", "Horizontal" },
+      v = { "<cmd>ToggleTerm size=80 direction=vertical<CR>", "Veritcal" },
+    },
 
-    j = { "<cmd>lua require('dap').down()<CR>", "Go Down Stacktrace" },
-    k = { "<cmd>lua require('dap').up()<CR>", "Go Up Stacktrace" },
-
-    l = { "<cmd>lua require('dap').run_last()<CR>", "Run Last" },
-    r = { "<cmd>lua require('dap').repl.toggle()<CR>", "Toggle REPL" },
-  },
-
-  --
-  -- ## Git
-  --
-  g = {
-    name = "Git",
-    f = { "<cmd>G<CR>", "Fugitive" },
-    g = { "<cmd>lua _GITUI_TOGGLE()<CR>", "GitUI" },
-    j = { "<cmd>lua require('gitsigns').next_hunk()<CR>", "Next Hunk" },
-    k = { "<cmd>lua require('gitsigns').prev_hunk()<CR>", "Prev Hunk" },
-    l = { "<cmd>lua require('gitsigns').blame_line()<CR>", "Blame" },
-    p = { "<cmd>lua require('gitsigns').preview_hunk()<CR>", "Preview Hunk" },
-    r = { "<cmd>lua require('gitsigns').reset_hunk()<CR>", "Reset Hunk" },
-    R = { "<cmd>lua require('gitsigns').reset_buffer()<CR>", "Reset Buffer" },
-    s = { "<cmd>lua require('gitsigns').stage_hunk()<CR>", "Stage Hunk" },
-    u = { "<cmd>lua require('gitsigns').undo_stage_hunk()<CR>", "Undo Stage Hunk" },
-    o = { "<cmd>Telescope git_status<CR>", "Open Changed File" },
-    b = { "<cmd>Telescope git_branches<CR>", "Checkout Branch" },
-    c = { "<cmd>Telescope git_commits<CR>", "Checkout Commit" },
-    d = { "<cmd>Gitsigns diffthis HEAD<CR>", "Diff" },
-  },
-
-  --
-  -- ## LSP
-  --
-  l = {
-    name = "LSP",
-    i = { "<cmd>LspInfo<CR>", "Info" },
-    I = { "<cmd>LspInstallInfo<CR>", "Installer Info" },
-    -- For activated buffer-local mappings, see `lua/my/lsp/handlers.lua`
-  },
-
-  --
-  -- ## Terminal
-  --
-  m = {
-    name = "Terminal",
-    b = { "<cmd>lua _HTOP_TOGGLE()<CR>", "Htop" },
-    f = { "<cmd>ToggleTerm direction=float<CR>", "Float" },
-    h = { "<cmd>ToggleTerm size=10 direction=horizontal<CR>", "Horizontal" },
-    v = { "<cmd>ToggleTerm size=80 direction=vertical<CR>", "Veritcal" },
-  },
-
-  --
-  -- ## Packer
-  --
-  p = {
-    name = "Packer",
-    c = { "<cmd>PackerCompile<CR>", "Compile" },
-    i = { "<cmd>PackerInstall<CR>", "Install" },
-    s = { "<cmd>PackerSync<CR>", "Sync" },
-    S = { "<cmd>PackerStatus<CR>", "Status" },
-    u = { "<cmd>PackerUpdate<CR>", "Update" },
-  },
-
-  --
-  -- ## Search
-  --
-  s = {
-    name = "Search",
-    b = { "<cmd>Telescope git_branches<CR>", "Checkout Branch" },
-    c = { "<cmd>Telescope colorscheme<CR>", "Colorscheme" },
-    f = { "<cmd>Telescope find_files<CR>", "Find File" },
-    h = { "<cmd>Telescope help_tags<CR>", "Find Help" },
-    m = { "<cmd>Telescope man_pages<CR>", "Man Pages" },
-    r = { "<cmd>Telescope oldfiles<CR>", "Open Recent File" },
-    R = { "<cmd>Telescope registers<CR>", "Register" },
-    t = { "<cmd>Telescope live_grep theme=ivy<CR>", "Text" },
-    k = { "<cmd>Telescope keymaps<CR>", "Key Mapping" },
-    C = { "<cmd>Telescope commands<CR>", "Commands" },
+    --
+    -- ## Packer
+    --
     p = {
-      "<cmd>lua require('telescope.builtin.internal').colorscheme({ enable_preview = true })<CR>",
-      "Colorscheme With Preview",
+      name = "Packer",
+      c = { "<cmd>PackerCompile<CR>", "Compile" },
+      i = { "<cmd>PackerInstall<CR>", "Install" },
+      s = { "<cmd>PackerSync<CR>", "Sync" },
+      S = { "<cmd>PackerStatus<CR>", "Status" },
+      u = { "<cmd>PackerUpdate<CR>", "Update" },
     },
-  },
 
-  -- ## Treesitter
-  --
-  S = {
-    name = "Treesitter",
-    i = { ":TSConfigInfo<CR>", "Info" },
-  },
+    --
+    -- ## Trouble
+    --
+    r = {
+      name = "Trouble",
+      d = { "<cmd>Trouble document_diagnostics<CR>", "Toggle Document Diagnostics" },
+      D = { "<cmd>Trouble lsp_definitions<CR>", "LSP Definitions" },
+      l = { "<cmd>Trouble loclist<CR>", "Location List" },
+      q = { "<cmd>Trouble quickfix<CR>", "QuickFix" },
+      r = { "<cmd>TroubleRefresh<CR>", "Refresh Diagnostics" },
+      R = { "<cmd>Trouble lsp_references<CR>", "LSP References" },
+      w = { "<cmd>Trouble workspace_diagnostics<CR>", "Toggle Workspace Diagnostics" },
+    },
 
-  --
-  -- ## Trouble
-  --
-  t = {
-    name = "Trouble",
-    d = { "<cmd>Trouble document_diagnostics<CR>", "Toggle Document Diagnostics" },
-    D = { "<cmd>Trouble lsp_definitions<CR>", "LSP Definitions" },
-    l = { "<cmd>Trouble loclist<CR>", "Location List" },
-    q = { "<cmd>Trouble quickfix<CR>", "QuickFix" },
-    r = { "<cmd>TroubleRefresh<CR>", "Refresh Diagnostics" },
-    R = { "<cmd>Trouble lsp_references<CR>", "LSP References" },
-    w = { "<cmd>Trouble workspace_diagnostics<CR>", "Toggle Workspace Diagnostics" },
-  },
+    --
+    -- ## Search
+    --
+    s = {
+      name = "Search",
+      b = { "<cmd>Telescope git_branches<CR>", "Checkout Branch" },
+      c = { "<cmd>Telescope colorscheme<CR>", "Colorscheme" },
+      f = { "<cmd>Telescope find_files<CR>", "Find File" },
+      h = { "<cmd>Telescope help_tags<CR>", "Find Help" },
+      m = { "<cmd>Telescope man_pages<CR>", "Man Pages" },
+      r = { "<cmd>Telescope oldfiles<CR>", "Open Recent File" },
+      R = { "<cmd>Telescope registers<CR>", "Register" },
+      t = { "<cmd>Telescope live_grep theme=ivy<CR>", "Text" },
+      k = { "<cmd>Telescope keymaps<CR>", "Key Mapping" },
+      C = { "<cmd>Telescope commands<CR>", "Commands" },
+      p = {
+        "<cmd>lua require('telescope.builtin.internal').colorscheme({ enable_preview = true })<CR>",
+        "Colorscheme With Preview",
+      },
+    },
 
-  --
-  -- ## Toggle
-  --
-  T = {
-    name = "Toggle",
-    d = { "<cmd>lua require('dapui').toggle()<CR>", "DAP UI" },
-    t = { "<cmd>TransparentToggle<CR>", "Transparent" },
-    T = { "<cmd>TroubleToggle<CR>", "Trouble" },
+    -- ## Treesitter
+    --
+    S = {
+      name = "Treesitter",
+      i = { ":TSConfigInfo<CR>", "Info" },
+    },
+
+    --
+    -- ## Toggle
+    --
+    T = {
+      name = "Toggle",
+      d = { "<cmd>lua require('dapui').toggle()<CR>", "DAP UI" },
+      t = { "<cmd>TransparentToggle<CR>", "Transparent" },
+      T = { "<cmd>TroubleToggle<CR>", "Trouble" },
+    },
   },
 }
 
@@ -260,4 +262,6 @@ local opts = {
 }
 
 which_key.setup(setup)
-which_key.register(mappings, opts)
+for prefix, global_mappings in pairs(mappings) do
+  which_key.register(global_mappings, vim.tbl_deep_extend("force", opts, { prefix = prefix }))
+end
