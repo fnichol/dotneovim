0a6a2eb3b681e62e411e5be9619455b6a5417246
Fletcher Nichol <fnichol@nichol.ca>
2022-02-19 17:00:17 -0700

    feat: add support for AWK
    Signed-off-by: Fletcher Nichol <fnichol@nichol.ca>

---
+++ home/.config/nvim/ftplugin/awk.lua
@@ -0,0 +1,5 @@
+local colorcolumn = 80
+require("my.utils").setlocal_colorcolumn(colorcolumn)
+
+vim.opt_local.shiftwidth = 4
+vim.opt_local.softtabstop = 4
--- home/.config/nvim/lua/my/lsp/lsp-installer.lua
+++ home/.config/nvim/lua/my/lsp/lsp-installer.lua
@@ -13,6 +13,11 @@ lsp_installer.on_server_ready(function(server)
     capabilities = require("my.lsp.handlers").capabilities,
   }
 
+  if server.name == "awk_ls" then
+    local awk_ls_opts = require("my.lsp.settings.awk_ls")
+    opts = vim.tbl_deep_extend("force", opts, awk_ls_opts)
+  end
+
   if server.name == "jsonls" then
     local jsonls_opts = require("my.lsp.settings.jsonls")
     opts = vim.tbl_deep_extend("force", opts, jsonls_opts)
