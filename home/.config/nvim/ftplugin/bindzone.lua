f35d9518d989a9496c8810474c25f95971188660
Fletcher Nichol <fnichol@nichol.ca>
2022-02-19 17:04:47 -0700

    feat: add support for BIND zone files
    Signed-off-by: Fletcher Nichol <fnichol@nichol.ca>

---
+++ home/.config/nvim/ftplugin/bindzone.lua
@@ -0,0 +1,7 @@
+local colorcolumn = 80
+require("my.utils").setlocal_colorcolumn(colorcolumn)
+
+vim.opt_local.tabstop = 8
+vim.opt_local.shiftwidth = 8
+vim.opt_local.softtabstop = 8
+vim.opt_local.expandtab = false
