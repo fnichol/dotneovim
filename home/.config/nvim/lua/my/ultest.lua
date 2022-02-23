0be73af1cd09d9553a56730899dfe906b73b42f5
Fletcher Nichol <fnichol@nichol.ca>
2022-02-22 18:49:30 -0700

    fix(ultest): ensure that `pynvim` Python3 provider is installed
    Signed-off-by: Fletcher Nichol <fnichol@nichol.ca>

--- home/.config/nvim/lua/my/ultest.lua
+++ home/.config/nvim/lua/my/ultest.lua
@@ -4,3 +4,7 @@ local require_or_warn = require("my.utils").require_or_warn
 
 vim.api.nvim_exec([[let test#rust#cargotest#options = "--color=always"]], false)
 vim.api.nvim_exec([[let test#strategy = "neovim"]], false)
+
+-- Schedules the detection execution to run on the event loop so as to not
+-- block when this module is loaded in on boot
+vim.defer_fn(require("my.install.pynvim").add_install_command_if_not_detected, 0)
