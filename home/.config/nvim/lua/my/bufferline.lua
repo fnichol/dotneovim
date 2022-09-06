8ccf1e5efbf5cf54dccfa46105b42409afacd822
Fletcher Nichol <fnichol@nichol.ca>
2022-09-06 14:53:15 -0600

    chore(bufferline): update configuration to resolve deprecated usage
    References: akinsho/bufferline.nvim#514
    
    Signed-off-by: Fletcher Nichol <fnichol@nichol.ca>

--- home/.config/nvim/lua/my/bufferline.lua
+++ home/.config/nvim/lua/my/bufferline.lua
@@ -19,10 +19,9 @@ bufferline.setup({
     left_mouse_command = "buffer %d",
     -- Can be a string or a function, see "Mouse actions"
     middle_mouse_command = nil,
-    -- NOTE: this plugin is designed with this icon in mind,
-    -- and so changing this is NOT recommended, this is intended
-    -- as an escape hatch for people who cannot bear it for whatever reason
-    indicator_icon = "▎",
+    indicator = {
+      icon = "▎",
+    },
     buffer_close_icon = "",
     -- buffer_close_icon = '',
     modified_icon = "●",
