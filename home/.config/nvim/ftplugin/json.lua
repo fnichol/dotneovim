922ffc938d432c12a4c341fcf01764bd2f76a510
Fletcher Nichol <fnichol@nichol.ca>
2023-02-08 14:01:58 -0700

    chore: remove install logic for plugins avail via mason
    This change drops detection and installation logic for the following
    tools:
    
    - jsonlint
    - yamllint
    - prettier
    
    All of these tools are now installed and managed by mason.nvim
    
    Signed-off-by: Fletcher Nichol <fnichol@nichol.ca>

--- home/.config/nvim/ftplugin/json.lua
+++ home/.config/nvim/ftplugin/json.lua
@@ -1,7 +1,2 @@
 local colorcolumn = 80
 require("my.utils").setlocal_colorcolumn(colorcolumn)
-
--- Schedules the detection execution to run on the event loop so as to not
--- block when this module is loaded in on boot
-vim.defer_fn(require("my.install.jsonlint").add_install_command_if_not_detected())
-vim.defer_fn(require("my.install.prettier").add_install_command_if_not_detected())
--- home/.config/nvim/ftplugin/yaml.lua
+++ home/.config/nvim/ftplugin/yaml.lua
@@ -5,5 +5,3 @@ vim.opt_local.tabstop = 8
 vim.opt_local.shiftwidth = 2
 vim.opt_local.softtabstop = 2
 vim.opt_local.expandtab = true
-
-require("my.install.yamllint").add_install_command_if_not_detected()
