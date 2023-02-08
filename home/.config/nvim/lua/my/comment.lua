ea16070afd1260549174c230165a209a75fc3193
Fletcher Nichol <fnichol@nichol.ca>
2023-02-08 12:38:09 -0700

    chore: update Comment configuration to stock/defaults
    Signed-off-by: Fletcher Nichol <fnichol@nichol.ca>

--- home/.config/nvim/lua/my/comment.lua
+++ home/.config/nvim/lua/my/comment.lua
@@ -1,25 +1,10 @@
-local ok, comment = pcall(require, "Comment")
+local require_or_warn = require("my.utils").require_or_warn
+
+local ok, comment = require_or_warn("Comment")
 if not ok then
-  vim.notify("[my.comment] failed to require 'Comment'", vim.log.levels.WARN)
   return
 end
 
 -- Usage: https://github.com/numToStr/Comment.nvim
 
-comment.setup({
-  pre_hook = function(ctx)
-    local U = require("Comment.utils")
-
-    local location = nil
-    if ctx.ctype == U.ctype.block then
-      location = require("ts_context_commentstring.utils").get_cursor_location()
-    elseif ctx.cmotion == U.cmotion.v or ctx.cmotion == U.cmotion.V then
-      location = require("ts_context_commentstring.utils").get_visual_start_location()
-    end
-
-    return require("ts_context_commentstring.internal").calculate_commentstring({
-      key = ctx.ctype == U.ctype.line and "__default" or "__multiline",
-      location = location,
-    })
-  end,
-})
+comment.setup()
