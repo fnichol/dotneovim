4f44fa9dc6cdc46808bc39fbb30f78a05e262b53
Fletcher Nichol <fnichol@nichol.ca>
2023-02-08 12:09:16 -0700

    chore(rust): clean up ftplugin for Rust
    Signed-off-by: Fletcher Nichol <fnichol@nichol.ca>

--- home/.config/nvim/ftplugin/rust.lua
+++ home/.config/nvim/ftplugin/rust.lua
@@ -1,6 +1,3 @@
-local utils = require("my.utils")
-local require_or_warn = utils.require_or_warn
-
 -- https://github.com/rust-dev-tools/fmt-rfcs/blob/master/guide/guide.md
 local colorcolumn = 100
-utils.setlocal_colorcolumn(colorcolumn)
+require("my.utils").setlocal_colorcolumn(colorcolumn)
