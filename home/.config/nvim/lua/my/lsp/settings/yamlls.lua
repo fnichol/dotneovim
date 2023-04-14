60b16c212e05b2c584d0a8f94466c3c637ab6cbf
Fletcher Nichol <fnichol@nichol.ca>
2023-04-14 16:41:25 -0600

    chore(lsp): temporarily override default setting in `yamlls`
    The next release of yaml-language-server (post-1.12.0) will set the
    `keyOrdering` setting to `false` by default.
    
    References: redhat-developer/yaml-language-server#859
    
    Signed-off-by: Fletcher Nichol <fnichol@nichol.ca>

--- home/.config/nvim/lua/my/lsp/settings/yamlls.lua
+++ home/.config/nvim/lua/my/lsp/settings/yamlls.lua
@@ -1,3 +1,12 @@
 -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#yamlls
 
-return {}
+return {
+  settings = {
+    yaml = {
+      -- TODO: remove on next release (post-1.12.0) when default for this setting is `false`
+      --
+      -- Reference: https://github.com/redhat-developer/yaml-language-server/pull/859
+      keyOrdering = false
+    }
+  }
+}
