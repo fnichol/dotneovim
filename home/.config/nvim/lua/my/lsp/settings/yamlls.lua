3d8bcc9613334fc00f756554efceb015177c3dc7
Fletcher Nichol <fnichol@nichol.ca>
2023-04-18 13:59:32 -0600

    style(lsp): update formatting via stylua
    Signed-off-by: Fletcher Nichol <fnichol@nichol.ca>

--- home/.config/nvim/lua/my/lsp/settings/yamlls.lua
+++ home/.config/nvim/lua/my/lsp/settings/yamlls.lua
@@ -6,7 +6,7 @@ return {
       -- TODO: remove on next release (post-1.12.0) when default for this setting is `false`
       --
       -- Reference: https://github.com/redhat-developer/yaml-language-server/pull/859
-      keyOrdering = false
-    }
-  }
+      keyOrdering = false,
+    },
+  },
 }
