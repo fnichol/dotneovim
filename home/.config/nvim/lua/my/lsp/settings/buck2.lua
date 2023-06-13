10ed08067fdeda090422850f3c59b6a310bb7d70
Fletcher Nichol <fnichol@nichol.ca>
2023-06-13 11:58:19 -0600

    feat(lsp): use a dedication isolation dir for the Buck2 LSP
    This keeps the LSP running with its own instance, even when calling
    operations such as `buck2 kill` or `buck2 clean` on the commandline.
    
    Signed-off-by: Fletcher Nichol <fnichol@nichol.ca>

--- home/.config/nvim/lua/my/lsp/settings/buck2.lua
+++ home/.config/nvim/lua/my/lsp/settings/buck2.lua
@@ -1,3 +1,13 @@
 -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#buck2
 
-return {}
+return {
+  -- Use a seperate isolation dir (default: `v2`) for the Buck2 LSP. That way,
+  -- cleaning/killing the buckd on the commandline won't kill the running LSP
+  -- instance as well.
+  cmd = {
+    "buck2",
+    "--isolation-dir",
+    "neovim",
+    "lsp",
+  },
+}
