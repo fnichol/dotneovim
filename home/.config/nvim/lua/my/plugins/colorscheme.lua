89e9a089d56c880ed033e8b52bb3938e86e492e0
Fletcher Nichol <fnichol@nichol.ca>
2024-07-25 19:47:34 -0600

    feat: setup colorscheme
    Signed-off-by: Fletcher Nichol <fnichol@nichol.ca>

---
+++ home/.config/nvim/lua/my/plugins/colorscheme.lua
@@ -0,0 +1,22 @@
+return {
+  -- Soothing pastel theme for (Neo)vim
+  --
+  -- https://github.com/catppuccin/nvim
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    lazy = false,
+    priority = 1000, -- Ensure this plugin loads before all other start plugins
+    init = function()
+      vim.cmd.colorscheme "catppuccin-mocha"
+    end,
+  },
+  -- Remove all background colors to make nvim transparent
+  --
+  -- https://github.com/xiyaowong/transparent.nvim
+  {
+    "xiyaowong/nvim-transparent",
+    lazy = false,
+    opts = {},
+  },
+}
