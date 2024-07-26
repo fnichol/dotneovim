20fd83b7d7c093fedb9452c33aa6d1b22c27deb9
Fletcher Nichol <fnichol@nichol.ca>
2024-07-25 19:48:03 -0600

    feat: setup which-key for key binding discovery
    Signed-off-by: Fletcher Nichol <fnichol@nichol.ca>

---
+++ home/.config/nvim/lua/my/plugins/which-key.lua
@@ -0,0 +1,35 @@
+return {
+  -- Create key bindings that stick.
+  --
+  -- https://github.com/folke/which-key.nvim
+  {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    dependencies = {
+      { "nvim-tree/nvim-web-devicons" },
+      { "echasnovski/mini.icons" },
+    },
+    opts = {},
+    keys = {
+      {
+        "<leader>?",
+	function()
+          require("which-key").show({ global = false })
+	end,
+	desc = "Buffer Local Keymaps (which-key)",
+      },
+    },
+    config = function()
+      local wk = require("which-key")
+
+      wk.setup({})
+
+      wk.add({
+        { "<leader>g", group = "Git" },
+        { "<leader>i", group = "Installers" },
+        { "<leader>il", group = "Lazy" },
+        { "<leader>s", group = "Search" },
+      })
+    end,
+  },
+}
