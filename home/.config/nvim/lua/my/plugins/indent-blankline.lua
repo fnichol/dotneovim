c5b57f742dd3854bd6f85077fc48ae71f1eaa1a3
Fletcher Nichol <fnichol@nichol.ca>
2024-07-30 15:29:33 -0600

    feat: add indentation guides via `indent-blankline` plugin
    Signed-off-by: Fletcher Nichol <fnichol@nichol.ca>

---
+++ home/.config/nvim/lua/my/plugins/indent-blankline.lua
@@ -0,0 +1,11 @@
+return {
+  -- Indent guides for Neovim
+  --
+  -- https://github.com/lukas-reineke/indent-blankline.nvim
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    -- Enable plugin, see: `:help ibl`
+    main = "ibl",
+    opts = {},
+  },
+}
