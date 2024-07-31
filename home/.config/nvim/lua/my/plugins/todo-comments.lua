7e2b2fd84d031c5143bca4ef32c1ae949b158950
Fletcher Nichol <fnichol@nichol.ca>
2024-07-31 02:55:48 -0600

    chore(todo): add keymaps
    Signed-off-by: Fletcher Nichol <fnichol@nichol.ca>

--- home/.config/nvim/lua/my/plugins/todo-comments.lua
+++ home/.config/nvim/lua/my/plugins/todo-comments.lua
@@ -7,13 +7,35 @@ return {
     event = "VimEnter",
     dependencies = {
       -- full; complete; entire; absolute; unqualified. All the lua functions I
-      -- don't want to write twice. 
+      -- don't want to write twice.
       --
       -- https://github.com/nvim-lua/plenary.nvim
       { "nvim-lua/plenary.nvim" },
     },
     opts = {
       signs = false,
-    }
+    },
+    cmd = { "TodoTrouble", "TodoTelescope" },
+    keys = {
+      {
+        "]t",
+        function()
+          require("todo-comments").jump_next()
+        end,
+        desc = "Next Todo Comment",
+      },
+      {
+        "[t",
+        function()
+          require("todo-comments").jump_prev()
+        end,
+        desc = "Previous Todo Comment",
+      },
+      {
+        "<leader>sT",
+        "<cmd>TodoTelescope keywords=TODO,FIX,FIXME<CR>",
+        desc = "Search Todo/Fix/Fixme",
+      },
+    },
   },
 }
