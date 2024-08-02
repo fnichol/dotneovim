a607fe8254640cbbda7982798933764a7726e7e5
Fletcher Nichol <fnichol@nichol.ca>
2024-08-02 08:42:39 -0600

    fix(trouble): add missing field to show documentation
    Signed-off-by: Fletcher Nichol <fnichol@nichol.ca>

--- home/.config/nvim/lua/my/plugins/trouble.lua
+++ home/.config/nvim/lua/my/plugins/trouble.lua
@@ -11,32 +11,32 @@ return {
       {
         "<leader>rl",
         "<cmd>Trouble lsp toggle focus=false win.position=right<CR>",
-        "LSP refs/defs/... (Trouble)",
+        desc = "LSP refs/defs/... (Trouble)",
       },
       {
         "<leader>rL",
         "<cmd>Trouble loclist toggle<CR>",
-        "Location List (Trouble)",
+        desc = "Location List (Trouble)",
       },
       {
         "<leader>rq",
         "<cmd>Trouble qflist toggle<CR>",
-        "Quickfix List (Trouble)",
+        desc = "Quickfix List (Trouble)",
       },
       {
         "<leader>rs",
         "<cmd>Trouble symbols toggle focus=false<CR>",
-        "Symbols (Trouble)",
+        desc = "Symbols (Trouble)",
       },
       {
         "<leader>rx",
         "<cmd>Trouble diagnostics toggle<CR>",
-        "Diagnostics (Trouble)",
+        desc = "Diagnostics (Trouble)",
       },
       {
         "<leader>rX",
         "<cmd>Trouble diagnostics toggle filter.buf=0<CR>",
-        "Buffer Diagnostics (Trouble)",
+        desc = "Buffer Diagnostics (Trouble)",
       },
       {
         "[q",
