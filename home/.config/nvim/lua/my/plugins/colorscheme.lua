00734ada76f2a57345481fdfa6cbea182a3a2b86
Fletcher Nichol <fnichol@nichol.ca>
2024-08-06 13:46:56 -0600

    feat: improve colorscheme experience
    This change adds live preview to the telescope plugin, extracts the
    default colorscheme to a lua variable and groups all colorscheme plugins
    together.
    
    Signed-off-by: Fletcher Nichol <fnichol@nichol.ca>

--- home/.config/nvim/lua/my/plugins/colorscheme.lua
+++ home/.config/nvim/lua/my/plugins/colorscheme.lua
@@ -1,14 +1,44 @@
+local default_colorscheme = "catppuccin-macchiato"
+
 return {
-  -- Soothing pastel theme for (Neo)vim
-  --
-  -- https://github.com/catppuccin/nvim
+  -- Fake directory plugin to contain all colorscheme plugins and trigger the
+  -- current colorscheme
   {
-    "catppuccin/nvim",
-    name = "catppuccin",
+    dir = "my-colorschemes",
+    dependencies = {
+      -- Soothing pastel theme for (Neo)vim
+      --
+      -- https://github.com/catppuccin/nvim
+      {
+        "catppuccin/nvim",
+        name = "catppuccin",
+        lazy = false,
+        priority = 1000, -- Ensure this plugin loads before all other start plugins
+      },
+      -- A clean, dark Neovim theme written in Lua, with support for lsp,
+      -- treesitter and lots of plugins. Includes additional themes for Kitty,
+      -- Alacritty, iTerm and Fish
+      --
+      -- https://github.com/folke/tokyonight.nvim
+      {
+        "folke/tokyonight.nvim",
+        lazy = false,
+        priority = 1000, -- Ensure this plugin loads before all other start plugins
+        opts = {},
+      },
+      -- A dark, low-contrast, Vim colorscheme.
+      --
+      -- https://github.com/romainl/Apprentice
+      {
+        "romainl/Apprentice",
+        lazy = false,
+        priority = 1000, -- Ensure this plugin loads before all other start plugins
+      },
+    },
     lazy = false,
     priority = 1000, -- Ensure this plugin loads before all other start plugins
     init = function()
-      vim.cmd.colorscheme("catppuccin-mocha")
+      vim.cmd.colorscheme(default_colorscheme)
     end,
   },
   -- Remove all background colors to make nvim transparent
@@ -18,5 +48,6 @@ return {
     "xiyaowong/nvim-transparent",
     lazy = false,
     opts = {},
+    init = function() end,
   },
 }
