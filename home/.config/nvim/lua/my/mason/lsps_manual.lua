b7ac5b6046bc40e9a94c9f90ca4d68715ce9d4a7
Fletcher Nichol <fnichol@nichol.ca>
2024-10-28 16:40:20 -0600

    fix(lsp): fix `buck2` LSP cmd configuration
    Signed-off-by: Fletcher Nichol <fnichol@nichol.ca>

--- home/.config/nvim/lua/my/mason/lsps_manual.lua
+++ home/.config/nvim/lua/my/mason/lsps_manual.lua
@@ -6,17 +6,17 @@ local packages = {
   -- https://github.com/facebook/buck2
   buck2 = {
     -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#buck2
-    settings = {
-      -- Use a seperate isolation dir (default: `v2`) for the Buck2 LSP. That
-      -- way, cleaning/killing the buckd on the commandline won't kill the
-      -- running LSP instance as well.
-      cmd = {
-        "buck2",
-        "--isolation-dir",
-        "neovim",
-        "lsp",
-      },
+    --
+    -- Use a seperate isolation dir (default: `v2`) for the Buck2 LSP. That
+    -- way, cleaning/killing the buckd on the commandline won't kill the
+    -- running LSP instance as well.
+    cmd = {
+      "buck2",
+      "--isolation-dir",
+      "neovim",
+      "lsp",
     },
+    settings = {},
   },
   -- Tilt Language Server
   --
