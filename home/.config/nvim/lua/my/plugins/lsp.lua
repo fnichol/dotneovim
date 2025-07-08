00328a62d0d369c47ab6370b3eb4bcc6bbcbe08c
Fletcher Nichol <fnichol@nichol.ca>
2025-07-08 11:40:50 -0600

    chore(lsp): update lazydev config & remove luvit-meta plugin
    Signed-off-by: Fletcher Nichol <fnichol@nichol.ca>

--- home/.config/nvim/lua/my/plugins/lsp.lua
+++ home/.config/nvim/lua/my/plugins/lsp.lua
@@ -422,14 +422,10 @@ return {
     ft = "lua",
     opts = {
       library = {
-        { path = "luvit-meta/library", words = { "vim%.uv" } },
+        { path = "${3rd}/luv/library", words = { "vim%.uv" } },
       },
     },
   },
-  -- Meta type definitions for the Lua platform Luvit.
-  --
-  -- https://github.com/Bilal2453/luvit-meta
-  { "Bilal2453/luvit-meta", lazy = true },
   -- VSCode 💡 for neovim's built-in LSP.
   --
   -- https://github.com/kosayoda/nvim-lightbulb
