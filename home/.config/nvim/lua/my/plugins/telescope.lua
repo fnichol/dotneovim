61930eda65e56cd796e1b3f4a5de37caae9a2536
Fletcher Nichol <fnichol@nichol.ca>
2026-03-06 11:14:32 -0700

    fix(telescope): update to use latest released version
    References: nvim-telescope/telescope.nvim#3611
    
    Signed-off-by: Fletcher Nichol <fnichol@nichol.ca>

--- home/.config/nvim/lua/my/plugins/telescope.lua
+++ home/.config/nvim/lua/my/plugins/telescope.lua
@@ -10,7 +10,7 @@ return {
   {
     "nvim-telescope/telescope.nvim",
     event = "VimEnter",
-    branch = "0.1.x",
+    version = "*",
     dependencies = {
       -- full; complete; entire; absolute; unqualified. All the lua functions I
       -- don't want to write twice.
